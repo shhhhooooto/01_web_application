650d7d03d184ee1c01b7a06f5dbbdc60ffd1fe76
shhhhooooto <818shouto.sakuma@gmail.com>
2025-04-08 21:57:53 +0900

    first

--- 00_basic.py
+++ 00_basic.py
@@ -1,7 +1,7 @@
-# import streamlit as st
+import streamlit as st
 
-# st.title("簡単なシミュレーション")
-# value = st.slider("数値を選んでね", 0, 100, 50)
-# st.write(f"あなたが選んだ数値の2倍は {value * 2} です！")
+st.title("簡単なシミュレーション")
+value = st.slider("数値を選んでね", 0, 100, 50)
+st.write(f"あなたが選んだ数値の2倍は {value * 2} です！")
 
 print("hello world!")
